ef2c55bbf78250c58a15711ec58e0c3ff09d0417
Otto Giron <ottog2486@gmail.com>
2014-02-20 23:20:50 -0600

    Integrated simple web server on osgi for testing purposes. Next web sockets  coming :)

--- jcr-livereload-server/src/main/java/com/jumlabs/jcr/livereload/server/Activator.java
+++ jcr-livereload-server/src/main/java/com/jumlabs/jcr/livereload/server/Activator.java
@@ -1,21 +1,33 @@
 package com.jumlabs.jcr.livereload.server;
 
+import org.glassfish.grizzly.http.server.HttpServer;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Activator implements BundleActivator {
-	Logger log = LoggerFactory.getLogger(Activator.class);
-
+	Logger logger = LoggerFactory.getLogger(Activator.class);
+	private HttpServer server;
 	public void start(BundleContext context) throws Exception {
-	  log.info("Initializing jetty server");
+		// TODO Auto-generated method stub
+		logger.info("Starting grizly server");
+		server = HttpServer.createSimpleServer("/var/www", 8080);
+		try {
+		    server.start();
+		    System.out.println("Press any key to stop the server...");
+		    System.in.read();
+		} catch (Exception e) {
+		    logger.error(e.toString());
+		}
+		
 
 	}
 
 	public void stop(BundleContext context) throws Exception {
 		// TODO Auto-generated method stub
-
+	   logger.info("Stoping the web server");
+	   server.shutdown();
 	}
 
 }
